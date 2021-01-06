27d64aec7ae354f8f46f3ea836479a1e3ff2dbe8
Kevin Cotrina <kcotrinam92@gmail.com>
2021-01-06 12:47:05 -0500

    Add functionality for changing text dialogs, options and question

--- dom.js
+++ dom.js
@@ -1,15 +1,73 @@
+let submitBtn = document.querySelector('#submit');
 
-function sheTalks() {
-    var elem = document.getElementById("animate");
-    var pos = 0;
-    var id = setInterval(frame, 5);
-    function frame() {
-        if (pos == 350) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + "px";
-            elem.style.left = pos + "px";
-        }
+const lesson1 = {
+    question: "What's your name?",
+    options: [
+        "This is the first option",
+        "Jose",
+        "Martin"
+    ],
+    dialog: {
+        true: "Nice to meet you",
+        false: "Is that really your Name?"
+    },
+    hint: {
+        hint1: 'Your name is not Jose.',
+        hint2: 'Your name is not Martin.'
     }
 }
+
+// const selectedOption = async () => {
+//     let selected = ''
+//     let options = [...document.querySelectorAll('#option')];
+//     options.forEach(opt => {
+//         opt.addEventListener('click', e => {
+//             selected = e.target.innerText
+//         })
+//     })
+//     return selected
+// }
+
+
+const selectedAnswer = () => {
+    let selected = ''
+    let options = [...document.querySelectorAll('#option')];
+    options.forEach(opt => {
+        opt.addEventListener('click', e => {
+            selected = e.target.innerText
+            answerValidation(selected, lesson1)
+        })
+    })
+
+}
+
+selectedAnswer()
+
+const answerValidation = (answer, lesson) => {
+    if (answer == lesson.options[0]) {
+        console.log('hello')
+    } else if (answer == lesson.options[1]) {
+        console.log('hello2')
+    } else if (answer == lesson.options[2]) {
+        console.log('hello3')
+    } else {
+        console.log('no')
+    }
+
+}
+
+const changeQuestion = (question) => {
+    let questionContainer = document.querySelector('#questionContainer');
+    questionContainer.textContent = ''
+    questionContainer.textContent = question
+}
+
+//display the possible answers
+const changeOption = (newOptions) => {
+    let options = [...document.querySelectorAll('#option')];
+    options.forEach((el, idx) => {
+        el.textContent = ''
+        el.textContent = newOptions[idx]
+    })
+};
+
