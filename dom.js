05bc1ce8bc1499fe1ea3059b0e9b9c3017d6e595
Kevin Cotrina <kcotrinam92@gmail.com>
2021-01-07 10:15:26 -0500

    Refactor methods to use nested objects

--- dom.js
+++ dom.js
@@ -1,21 +1,23 @@
 let submitBtn = document.querySelector('#submit');
 
-const lesson1 = {
-    question: "Where are you from?",
-    options: {
-        invalid1: "This is the incorrect option1",
-        valid: "This is the correct option",
-        invalid2: "This is the incorrect option2"
-    },
-    dialog: {
-        true: "Nice to meet you",
-        false: "Is that really your Name?"
-    },
-    hint: {
-        hint1: 'Your name is not Jose.',
-        hint2: 'Your name is not Martin.'
+const module = [
+    {
+        question: "Where are you from?",
+        options: {
+            invalid1: "This is the incorrect option1",
+            valid: "This is the correct option",
+            invalid2: "This is the incorrect option2"
+        },
+        dialog: {
+            true: "Nice to meet you",
+            false: "Is that really your Name?"
+        },
+        hint: {
+            hint1: 'Your name is not Jose.',
+            hint2: 'Your name is not Martin.'
+        }
     }
-}
+]
 
 const changeQuestion = (question) => {
     let questionContainer = document.querySelector('#questionContainer');
@@ -24,50 +26,52 @@ const changeQuestion = (question) => {
 }
 
 //display the possible answers
-const changeOption = () => {
-    changeQuestion(lesson1.question)
+const changeOption = (idx) => {
+    changeQuestion(module[idx].question)
     let options = [...document.querySelectorAll('#option')];
     let counter = 0
-    for (let [key, value] of Object.entries(lesson1.options)) {
+    for (let [key, value] of Object.entries(module[idx].options)) {
         options[counter].textContent = value
         counter += 1
     }
 };
 
-const displayHint = (ans) => {
+const displayHint = (ans, lesson) => {
     let hintContainer = document.querySelector('#hint-container')
     hintContainer.textContent = ''
     if (ans == "invalid1") {
         hintContainer.classList.remove('hide');
-        hintContainer.textContent = lesson1.hint.hint1
+        hintContainer.textContent = lesson.hint.hint1
     } else if (ans == "invalid2") {
         hintContainer.classList.remove('hide');
-        hintContainer.textContent = lesson1.hint.hint2
+        hintContainer.textContent = lesson.hint.hint2
     } else {
         hintContainer.classList.add('hide');
     }
 }
 
-const displayDialog = (ans) => {
+const displayDialog = (ans, lesson) => {
     let dialogContainer = document.querySelector('#dialog-container')
     dialogContainer.textContent = ''
     if (ans == "correct") {
-        dialogContainer.textContent = lesson1.dialog.true
+        dialogContainer.textContent = lesson.dialog.true
     } else {
-        dialogContainer.textContent = lesson1.dialog.false
+        dialogContainer.textContent = lesson.dialog.false
     }
 }
 
+let counter = 0
+
 const selectedAnswer = () => {
-    changeOption()
+    changeOption(counter)
     let selected = ''
     let options = [...document.querySelectorAll('#option')];
     options.forEach(opt => {
         opt.addEventListener('click', e => {
             selected = e.target.innerText
-            let validatedAnswer = answerValidation(selected, lesson1)
-            displayDialog(validatedAnswer)
-            displayHint(validatedAnswer)
+            let validatedAnswer = answerValidation(selected, module[counter])
+            displayDialog(validatedAnswer, module[counter])
+            displayHint(validatedAnswer, module[counter])
         })
     })
 }
